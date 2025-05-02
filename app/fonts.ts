868a96fe157ca2823fd2b90bebfb03ff9cb8a6d6
kennde3 <nniyonshutikenny123@gmail.com>
2025-05-03 00:32:59 +0200

    fixing font error

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import { Poppins } from 'next/font/google';
-import { Righteous } from 'next/font/google';
+
 
 export const inter = Inter({
   subsets: ['latin'],
@@ -11,9 +11,4 @@ export const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '500', '600'],
   variable: '--font-poppins',
-});
-
-export const righteous = Righteous({
-  weight: '400',
-  subsets: ['latin'],
 });
