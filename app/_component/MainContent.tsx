dc0c0d608643d9a42f68cbc51ed4d11b5abe0787
kennde3 <nniyonshutikenny123@gmail.com>
2025-05-03 22:44:08 +0200

    _blank

--- app/_component/MainContent.tsx
+++ app/_component/MainContent.tsx
@@ -419,19 +419,19 @@ export default function MainContent() {
             </p>
             <div>
               <div className="flex space-x-6 items-center justify-center text-[#8892b0]">
-                <Link href="email:niyonshutikenny@yahoo.com" className="hover:text-[#64ffda] transition-colors">
+                <Link href="email:niyonshutikenny@yahoo.com" target="_blank" className="hover:text-[#64ffda] transition-colors">
                   <Mail size={20} />
                 </Link>
-                <Link href="https://github.com/kennde3" className="hover:text-[#64ffda] transition-colors">
+                <Link href="https://github.com/kennde3" target="_blank" className="hover:text-[#64ffda] transition-colors">
                   <Github size={20} />
                 </Link>
-                <Link href="https://www.linkedin.com/in/niyonshuti-kenny-5842632b8" className="hover:text-[#64ffda] transition-colors">
+                <Link href="https://www.linkedin.com/in/niyonshuti-kenny-5842632b8" target="_blank" className="hover:text-[#64ffda] transition-colors">
                   <Linkedin size={20} />
                 </Link>
-                <Link href="https://app.bag.work/niyonshuti_kenny558/" className="hover:text-[#64ffda] transition-colors">
+                <Link href="https://app.bag.work/niyonshuti_kenny558/" target="_blank" className="hover:text-[#64ffda] transition-colors">
                   <Bold size={20} />
                 </Link>
-                <Link href="https://twitter.com/isthat.kenny" className="hover:text-[#64ffda] transition-colors">
+                <Link href="https://twitter.com/isthat.kenny" target="_blank" className="hover:text-[#64ffda] transition-colors">
                   <Instagram size={20} />
                 </Link>
               </div>
